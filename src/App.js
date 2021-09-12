a65164f57cab35c45057f86664ff9f4812789fce
Asif <asifrmemon@gmail.com>
2021-09-12 13:38:17 -0700

    Adding in sign in and sign up components to App.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 import Header from './components/header/header.component'
 
 import './App.css';
-import { auth } from './firebase/firebase.utils'
+import { auth,  createUserProfileDocument } from './firebase/firebase.utils'
 
 const HatsPage = () => {
   return(
@@ -61,10 +61,28 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({ currentUser: user })
+      // createUserProfileDocument(user)
+      // console.log(user)
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state)
+          })
+        })
+      } else {
+        this.setState({
+          currentUser: userAuth
+        })
+      }
     })
   }
 
